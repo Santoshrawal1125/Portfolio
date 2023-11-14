8c570aff04eb479aa792c3aba4cac790d0e766c7
kabitarawal <kabitarawal88@gmail.com>
2023-11-14 15:01:30 -0500

    add santoshrawal.com.np on ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=^6r5gi8&v9$@t6b-8xi5y90-(8@74v&k@+ecwynlmx-m34w1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False  
+#make debug false, when you push new update to git as it auto deploy in production
 
-if DEBUG:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
-else:
-    ALLOWED_HOSTS = ['.vercel.app']
+# if DEBUG:
+#     ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
+# else:
+#     ALLOWED_HOSTS = ['.vercel.app']
     
-# ALLOWED_HOSTS = ["127.0.0.1", "portfolio-santosh.vercel.app", "https://portfolio-santosh.vercel.app/", "http://portfolio-santosh.vercel.app/"]
+ALLOWED_HOSTS = ["127.0.0.1", "portfolio-santosh.vercel.app", ".vercel.app", "santoshrawal.com.np"]
 
 
 # Application definition
