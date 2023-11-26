ba0b2d1f9f49da90f6debb197873569d8483bf94
Santosh Rawal <awesomekid.spa@gmail.com>
2023-11-26 20:09:31 +0545

    Download CV

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('project', ProjectDetailView.as_view(), name='project'),
-
+    path('download-cv/', download_cv, name='download_cv'),
 ]
